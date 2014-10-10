f3fdccf2b8291c6760b7592ec10b9e522291ea16
Alexandre Cordeiro <accordeiro@gmail.com>
2014-10-10 18:05:12 -0300

    Improvements on date parsing; Added url root for wikipedia pages

--- senator_scraper.js
+++ senator_scraper.js
@@ -12,26 +12,31 @@ if (wiki_url) {
       var senator_list = [];
 
       $row_list.each(function(index, row) {
-        var column_list   = $(row).find('td');
-        var senator_img   = $($(column_list[0]).find('a')[0]).attr('href');
-        var senator_page  = $($(column_list[0]).find('a')[1]).attr('href');
-        var senator_name  = $($(column_list[0]).find('a')[1]).text();
-        var senator_party = $(column_list[1]).text();
-        var senate_term   = $(column_list[2]).text();
+        if (index == 0) return;
+        var column_list    = $(row).find('td');
+        var senator_img    = $($(column_list[0]).find('a')[0]).attr('href');
+        var senator_page   = $($(column_list[0]).find('a')[1]).attr('href');
+        var senator_name   = $($(column_list[0]).find('a')[1]).text();
+        var senator_party  = $(column_list[1]).text();
+        var terms          = $(column_list[2]).text().split(' –\n');
+        var term_start     = terms[0]
+        var term_end       = terms[1]
+        var wikipedia_root = 'http://en.wikipedia.org'
 
         senator_list.push({
           "name"       : senator_name,
           "party"      : senator_party,
-          "wiki_url"   : senator_page,
-          "photo_url"  : senator_img,
-          "term"       : senate_term
+          "wiki_url"   : wikipedia_root + senator_page,
+          "photo_url"  : wikipedia_root + senator_img,
+          "term_start" : new Date(term_start),
+          "term_end"   : new Date(term_end)
         });
       });
 
-      console.log(senator_list);
+      console.log(JSON.stringify(senator_list, null, 4));
     }
 
   });
 } else {
-  console.log("Error: please provide a wiki url");
+  console.log('Error: please provide a wiki url');
 }
