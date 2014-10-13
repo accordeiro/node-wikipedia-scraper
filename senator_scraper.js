c9071d758c6d5a3e7ae30c1a7ebb570de67669bc
Alexandre Cordeiro <accordeiro@gmail.com>
2014-10-13 12:57:56 -0300

    Now able to export to CSV as well

--- senator_scraper.js
+++ senator_scraper.js
@@ -1,7 +1,20 @@
-var fs                = require('fs');
-var cheerio           = require('cheerio');
-var request           = require('request');
-var wiki_url          = process.argv[2];
+var cheerio    = require('cheerio');
+var fs         = require('fs');
+var json2csv   = require('json2csv');
+var request    = require('request');
+var wiki_url   = process.argv[2];
+var file_name  = process.argv[3];
+
+
+function fix_dates(term_start, term_end) {
+  var term_start = new Date(term_start);
+  var term_end   = new Date(term_end);
+
+  // Subtract one day from term_end to avoid overlapping
+  term_end.setDate(term_end.getDate() - 1);
+
+  return [term_start, term_end];
+}
 
 
 function parse_regular_row($, column_list, senate_class) {
@@ -10,8 +23,9 @@ function parse_regular_row($, column_list, senate_class) {
   var senator_name   = $($(column_list[0]).find('a')[1]).text();
   var senator_party  = $(column_list[1]).text();
   var terms          = $(column_list[2]).text().split(' –\n');
-  var term_start     = terms[0]
-  var term_end       = terms[1]
+  var term_dates     = fix_dates(terms[0], terms[1]);
+  var term_start     = term_dates[0];
+  var term_end       = term_dates[1];
   var wikipedia_root = 'http://en.wikipedia.org'
 
   senator_page = wikipedia_root + senator_page;
@@ -23,8 +37,8 @@ function parse_regular_row($, column_list, senate_class) {
     "senate_class" : senate_class,
     "wiki_url"     : senator_page,
     "photo_url"    : senator_img,
-    "term_start"   : new Date(term_start),
-    "term_end"     : new Date(term_end)
+    "term_start"   : term_start,
+    "term_end"     : term_end
   };
 
   return data_json;
@@ -33,17 +47,51 @@ function parse_regular_row($, column_list, senate_class) {
 
 function parse_vacant_row($, column_list, senate_class) {
   var terms      = $(column_list[1]).text().split(' –\n');
-  var term_start = terms[0];
-  var term_end   = terms[1];
+  var term_dates = fix_dates(terms[0], terms[1]);
+  var term_start = term_dates[0];
+  var term_end   = term_dates[1];
+
 
   var data_json = {
     "name"         : "Vacant",
-    "term_start"   : new Date(term_start),
-    "term_end"     : new Date(term_end),
+    "term_start"   : term_start,
+    "term_end"     : term_end,
     "senate_class" : senate_class
   };
 
   return data_json;
+}
+
+
+function export_to_csv(senator_list, file_name) {
+  json2csv({
+    data: senator_list,
+    fields: [
+      'name',
+      'party',
+      'senate_class',
+      'wiki_url',
+      'photo_url',
+      'term_start',
+      'term_end'
+    ]
+  },
+  function(err, csv) {
+    if (err) console.log(err);
+    fs.writeFile(file_name + '.csv', csv, function(err) {
+      if (err) throw err;
+      console.log('file saved');
+    });
+  });
+}
+
+
+function export_to_json(senator_list, file_name) {
+  var output = JSON.stringify(senator_list, null, 4);
+  fs.writeFile(file_name + '.json', output, function(err) {
+    if (err) throw err;
+    console.log('file saved');
+  });
 }
 
 
@@ -70,21 +118,24 @@ function main() {
             // Parse following rows:
             var data_json = {};
             var column_list = $(row).find('td');
-            if (column_list.length >= 4) {
+            if (column_list.length >= 5) {
               data_json = parse_regular_row($, column_list, class_id_list[i].senate_class);
+
             } else {
               data_json = parse_vacant_row($, column_list, class_id_list[i].senate_class);
             }
 
+            // Ensure the data retrieved is valid:
             if (!isNaN(data_json.term_start) && !isNaN(data_json.term_end)) {
               senator_list.push(data_json);
             }
           });
         };
 
-        console.log(JSON.stringify(senator_list, null, 4));
+        if (!file_name) { file_name = 'out' }
+        export_to_json(senator_list, file_name);
+        export_to_csv(senator_list, file_name);
       }
-
     });
   } else {
     console.log('Error: please provide a wiki url');
